24042aeecd3593768baa05c7f019e0fb792f7e36
tky_hm_xps13h <k_code@163.com>
2021-07-16 11:33:17 +0800

    [fix] cfg 科学计数法 字符串

--- cfg.py
+++ cfg.py
@@ -7,4 +7,9 @@ def parse_cfg(cfg_path):
         cfg = yaml.safe_load(f)
     cfg = EasyDict(cfg)
     
+    cfg.lr = float(cfg.lr)
+    cfg.wd = float(cfg.wd)
+    cfg.grad_clip = float(cfg.grad_clip)
+    cfg.fgm = float(cfg.fgm)
+    
     return cfg
